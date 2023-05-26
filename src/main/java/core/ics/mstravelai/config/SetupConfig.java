ce45d8d894a5d06b4e96634a41327a2047a08a77
Ivan Carlos dos Santos <112148249+eng-ivan@users.noreply.github.com>
2023-05-26 14:23:24 -0300

    Update SetupConfig.java
    change api key

--- src/main/java/core/ics/mstravelai/config/SetupConfig.java
+++ src/main/java/core/ics/mstravelai/config/SetupConfig.java
@@ -2,7 +2,7 @@ package core.ics.mstravelai.config;
 
 public class SetupConfig {
     private SetupConfig(){}
-    public static final String OPENAI_API_KEY = "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";
+    public static final String OPENAI_API_KEY = "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";
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKENS = 400;
     public static final Double TEMPERATURE = 0.6;
